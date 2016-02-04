a3085c39b4d108c42eafcfcdeff22420c4f6b290
Thomas Cuthbert <tcuthbert90@gmail.com>
2016-02-04 21:26:20 +0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+(function($) {
+    "use strict";
+
+	$.getJSON('https://api.github.com/users/tcuthbert', { get_param: 'value' }, function(data) {
+		$.each(data, function(index, element) {
+			if (index == "avatar_url") {
+				$('#avatar').append($('<img>', {
+					src: element
+				}));
+			}
+		});
+	});
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    $('#topNav').affix({
+        offset: {
+            top: 200
+        }
+    });
+    
+    new WOW().init();
+    
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    
+    $('.navbar-collapse ul li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggle:visible').click();
+    });
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+
+
+})(jQuery);
